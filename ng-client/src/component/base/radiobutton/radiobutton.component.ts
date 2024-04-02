619b1a7a14542051ba7e45fc1abff0610b8e9bc9
Harshit <prajapati.ha@northeastern.edu>
2024-04-02 15:41:58 -0400

    components commit

---
+++ ng-client/src/component/base/radiobutton/radiobutton.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radiobutton',
+  standalone: true,
+  imports: [],
+  templateUrl: './radiobutton.component.html',
+  styleUrl: './radiobutton.component.scss'
+})
+export class RadiobuttonComponent {
+
+}
